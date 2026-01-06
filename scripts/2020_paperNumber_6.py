1562f008c12ca4c38dbcf432c3e0fef9735546cb
Dineth Hirusha <dineth.hirusha.pro@gmail.com>
2026-01-06 06:28:56 +0530

    Add files via upload

---
+++ scripts/2020_paperNumber_6.py
@@ -0,0 +1,14 @@
+def cube_series(num):
+    if num == 1:
+        return 1
+    return cube_series(num-1) + num**3
+
+while True:
+    number = int(input("Enter a number: "))
+    if number == -1:
+        print("Output: Finished...")
+        break
+    elif number <= 0:
+        print("Invalid input. Please enter a positive integer.")
+    else:
+        print(f"Output: {cube_series(number)}")
